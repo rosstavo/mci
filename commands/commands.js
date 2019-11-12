9d2cd2abbeaa5001ed86c742a4596ae3aa37acac
Ross Hardy <ross@Giless-MacBook-Pro.local>
2019-11-12 18:11:01 +0200

    Fixing a typo

--- commands/commands.js
+++ commands/commands.js
@@ -5,8 +5,8 @@ module.exports = {
 
 		embed.setTitle( 'List of commands' )
 			.setColor(0xBBBBBB)
-			.addField( '!commands', 'This is the command you just did.' ),
-			.addField( '!day', 'Returns the in-Universe day.' ),
+			.addField( '!commands', 'This is the command you just did.' )
+			.addField( '!day', 'Returns the in-Universe day.' )
 			.addField( '!weather', 'Is it foggy? Try this command and see!' );
 
         msg.channel.send( embed );
