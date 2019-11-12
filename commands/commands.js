d096e0d3fc95e615508f23ce00ad14e5214109b3
Ross Hardy <ross@Giless-MacBook-Pro.local>
2019-11-12 18:08:54 +0200

    Adding commands command

---
+++ commands/commands.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: '!commands',
+	description: 'This command lists commands.',
+	execute(msg, args, embed) {
+
+		embed.setTitle( 'List of commands' )
+			.setColor(0xBBBBBB)
+			.addField( '!commands', 'This is the command you just did.' ),
+			.addField( '!day', 'Returns the in-Universe day.' ),
+			.addField( '!weather', 'Is it foggy? Try this command and see!' );
+
+        msg.channel.send( embed );
+
+	},
+};
--- commands/index.js
+++ commands/index.js
@@ -2,4 +2,5 @@ module.exports = {
     Request: require('./request'),
     Day: require('./day'),
     Weather: require('./weather'),
+    Commands: require('./commands'),
 };
