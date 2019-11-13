7487a5a9a5b0b77f40b3730760ca3769915c79f5
Ross Hardy <ross@Giless-MacBook-Pro.local>
2019-11-13 14:13:05 +0200

    Syntax error

--- commands/commands.js
+++ commands/commands.js
@@ -9,7 +9,7 @@ module.exports = {
 			.addField( '!day', 'Returns the in-Universe day.' )
 			.addField( '!weather', 'Is it foggy? Try this command and see!' )
 			.addField( '!dice', 'Forgotten how to roll with advantage? This’ll give you the lowdown.' )
-			.addField( '!safeword', 'For when things get too real. Use this command to notify the channel if you’re feeling overwhelmed. Your message will be deleted so anyone revisiting the conversation will not know who called timeout.' );
+			.addField( '!safeword', 'For when things get too real. Use this command to notify the channel if you’re feeling overwhelmed. Your message will be deleted so anyone revisiting the conversation will not know who called timeout.' )
 			.addField( '!player <character>', 'You can now look up player characters to see what level they are and some other info!' );
 
         msg.channel.send( embed );
