ae0a2e32ea887b691e6d21f4b0bcc75ae8417290
Ross Hardy <ross@Giless-MacBook-Pro.local>
2019-11-16 11:43:22 +0200

    Reordered commands

--- commands/commands.js
+++ commands/commands.js
@@ -4,13 +4,13 @@ module.exports = {
 	execute(msg, args, embed) {
 
 		embed.setTitle( 'List of directives' )
-			.addField( '!day', 'This directive returns the breather terminology for the current period in the 7-day cycle.' )
-			.addField( '!weather', 'This directive provides an analysis of the present meteorological phenomena.' )
-			.addField( '!dice', 'This directive provides training on correct syntax for random number generation via the rolling of dice.' )
-			.addField( '!safeword', 'This directive notifies the channel if the user is feeling overwhelmed. The user’s message will be deleted so any user revisiting the conversation will not know who called timeout.' )
 			.addField( '!character <character>', 'This directive returns intelligence on a specific player-directed inhabitant.' )
+			.addField( '!day', 'This directive returns the breather terminology for the current period in the 7-day cycle.' )
+			.addField( '!dice', 'This directive provides training on correct syntax for random number generation via the rolling of dice.' )
 			.addField( '!downtime <activity (optional)>', 'This directive returns an overview of the various activities users can undertake between missions.' )
-			.addField( '!status', 'This directive returns the current status of Intel. Officer DSGN. ‘Helper’.' );
+			.addField( '!safeword', 'This directive notifies the channel if the user is feeling overwhelmed. The user’s message will be deleted so any user revisiting the conversation will not know who called timeout.' )
+			.addField( '!status', 'This directive returns the current status of Intel. Officer DSGN. ‘Helper’.' )
+			.addField( '!weather', 'This directive provides an analysis of the present meteorological phenomena.' );
 
         msg.channel.send( embed );
 
