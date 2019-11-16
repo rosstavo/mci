d94c0a40c4722ee2fafcf1c40b8b316172fc6884
Ross Hardy <ross@Giless-MacBook-Pro.local>
2019-11-16 11:11:04 +0200

    Added status to commands

--- commands/commands.js
+++ commands/commands.js
@@ -9,7 +9,8 @@ module.exports = {
 			.addField( '!dice', 'This directive provides training on correct syntax for random number generation via the rolling of dice.' )
 			.addField( '!safeword', 'This directive notifies the channel if the user is feeling overwhelmed. The user’s message will be deleted so any user revisiting the conversation will not know who called timeout.' )
 			.addField( '!character <character>', 'This directive returns intelligence on a specific player-directed inhabitant.' )
-			.addField( '!downtime <activity (optional)>', 'This directive returns an overview of the various activities users can undertake between missions.' );
+			.addField( '!downtime <activity (optional)>', 'This directive returns an overview of the various activities users can undertake between missions.' )
+			.addField( '!status', 'This directive returns the current status of Intel. Officer DSGN. ‘Helper’.' );
 
         msg.channel.send( embed );
 
