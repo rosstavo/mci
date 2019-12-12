9c0b85bf4b7b4f0b6a2d947f1600f84080a38218
Ross Hardy <ross@Giless-MacBook-Pro.local>
2019-12-12 11:06:34 +0000

    Changed emoji on checkin

--- commands/checkin.js
+++ commands/checkin.js
@@ -9,15 +9,15 @@ module.exports = {
 			.setColor(0xe67e22)
 			.setDescription( 'When you or someone else says something and you aren’t sure if everyone is into it or if it’s the best idea, you can call a check in with this command. This can be used in both text RP and the chat channels.\n\nIf you are participating in or spectating the conversation, please react to this message in one of the following ways:\n' )
 			.addField( ':thumbsup:', 'If you think what was said was fine and doesn’t need to be revised', true )
-			.addField( ':raised_hand:', 'If you don’t know how you feel about what was said or don’t care', true )
-			.addField( ':thumbsdown:', '(In RP) If you think what was said is worth revising OR (In chat) if you would like to bring the topic of conversation to a close', true )
+			.addField( ':person_shrugging:', 'If you don’t know how you feel about what was said or don’t care', true )
+			.addField( ':arrows_counterclockwise:', '(In RP) If you think what was said is worth revising OR (In chat) if you would like to bring the topic of conversation to a close', true )
 			.setFooter( 'This was called via the !checkin command. The message calling the command has been deleted.' );
 
 		msg.channel.send( embed ).then(function (message) {
 
 			message.react('👍');
-			message.react('✋');
-			message.react('👎');
+			message.react('🤷');
+			message.react('🔄');
 
 		} );
 
