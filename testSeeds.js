9f1f1559bfc7cd1aedf22204c4f236eafc5b6efb
Ross <ross@Rosss-MacBook-Pro.local>
2020-05-13 18:22:26 +0100

    Update seed

--- testSeeds.js
+++ testSeeds.js
@@ -1,7 +1,7 @@
 const fn          = require('./functions.js');
 const shuffleSeed = require('shuffle-seed');
 
-let daySeed               = fn.daysBetween( new Date("May 11 2020 00:00:00 UTC"), new Date(new Date().toUTCString()) );
+let daySeed               = fn.daysBetween( new Date("May 12 2020 00:00:00 UTC"), new Date(new Date().toUTCString()) );
 let weekSeed              = Math.floor(daySeed / 7);
 let fortnightSeed         = Math.floor(daySeed / 14);
 
